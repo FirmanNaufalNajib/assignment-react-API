14efd58669082a989dd2a769d87dffeb7c47a1fe
Firman <firmannaufalnajib@gmail.com>
2024-03-09 10:42:11 +0700

    add UI render at Form.jsx

---
+++ mini-project/src/components/Form.jsx
@@ -0,0 +1,16 @@
+
+const Form = () => {
+
+  return (
+
+    <div id="register-box">
+    <input type="text" placeholder="Full Name"></input>
+    <input type="email" placeholder="Email"></input>
+    <input type="text" placeholder="Username"></input>
+    <input type="password" placeholder="Password"></input>
+    <input type="password" placeholder="Confirm Password"></input>
+    <button>Sign Up</button>
+    </div>
+    
+  )
+}
