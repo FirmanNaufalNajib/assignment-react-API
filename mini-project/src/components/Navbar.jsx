311b979a2a3089b33176d646b45b116335f7ac8f
Firman <firmannaufalnajib@gmail.com>
2024-03-16 21:05:39 +0700

    add button in Navbar

--- mini-project/src/components/Navbar.jsx
+++ mini-project/src/components/Navbar.jsx
@@ -1,12 +1,23 @@
 import { Link } from "react-router-dom"
+import { useNavigate } from "react-router-dom"
+import "./components.css"
 
 const Navbar = () => {
+  const backNavigate = useNavigate()
 
   return (
-  <>
-    <Link to="/"><h3>Home</h3></Link>
-    <Link to="/users"><h3>Users</h3></Link>  
-  </>
+    <>
+    <div className="navbar">
+      
+    <Link to="/" className="navbar-link">
+    <h3 >Home</h3>
+    </Link>
+    <Link to="/users" className="navbar-link">
+    <h3 >Users</h3>
+    </Link>
+    <button className="page-button" onClick={() => backNavigate(-1)}>BACK</button>
+    </div>    
+    </>
   )
 }
 
