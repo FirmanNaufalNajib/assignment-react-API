84409d2a4eed2b181b04c8612f57feb5c70a4131
Firman <firmannaufalnajib@gmail.com>
2024-03-16 21:07:44 +0700

    edit UserItem

--- mini-project/src/components/UsersItem.jsx
+++ mini-project/src/components/UsersItem.jsx
@@ -3,10 +3,11 @@ const UsersItem = (props) => {
 
   return (
     <>
-    <img src={props.avatar}/>
-    <h4>first name: {props.first_name}</h4>
-    <h4>last name: {props.last_name}</h4>
-    <p>email: {props.email}</p>
+    <div className="item-container">
+      <img src={props.avatar}/>
+      <h4>{props.first_name} {props.last_name}</h4>
+      <p>{props.email}</p>   
+    </div>
     </>
   )
 }
